4e666bdf7752fc595943423fba2386c0d7422878
Saaeesh-Pednekar <macincredible369@gmail.com>
2025-03-23 13:56:26 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -85,34 +85,43 @@ runs_by_player = runs_by_player.rename_axis('Batter')
 runs_by_player = runs_by_player.rename("Runs")
 
 run_value = st.sidebar.slider("Filter by Runs", value=(runs_by_player.min(), runs_by_player.max()))
-st.markdown(
-    """
-    <style>
-    div[role="slider"] div[data-baseweb="slider-thumb"] {
-        background-color: red; 
-    }
-    div[role="slider"] div[data-baseweb="slider-track"] {
-        background: linear-gradient(to right, red, red);
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-filtered_runs_by_player = pd.Series()
-# st.write(run_value[1])
+
 runs_by_player = runs_by_player.sort_values(ascending=False)
 if run_value[0] == runs_by_player.min() and run_value[1] == runs_by_player.max():
     filtered_runs_by_player = runs_by_player
 else:
-    # for runs in runs_by_player:
-    #     if runs >= run_value[0] and runs<= run_value[1]:
     runs_by_player = runs_by_player[ runs_by_player > run_value[0]]
     runs_by_player = runs_by_player[ runs_by_player < run_value[1]]
     filtered_runs_by_player = runs_by_player
     
             
-
-fig3 = px.bar(filtered_runs_by_player, x=filtered_runs_by_player.index, y='Runs', color='Runs', template='seaborn')
+color_sequence = ['green', 'salmon', 'lightgreen', 'orange']
+fig3 = px.bar(filtered_runs_by_player, x=filtered_runs_by_player.index, y='Runs', color = 'Runs', template='seaborn')
 fig3.update_traces(width = 3)
-st.plotly_chart(fig3, use_container_width=True, height = 300)
-# st.sidebar()
+st.plotly_chart(fig3, use_container_width=True, height = 500)
+
+
+
+wicket_by_player = df_match_data.groupby(by=['bowler'])['player_dismissed'].count()
+wicket_by_player = wicket_by_player.rename_axis('Bowler')
+wicket_by_player = wicket_by_player.rename('Wickets')
+
+
+
+# st.write(wicket_by_player)
+
+
+wicket_value = st.sidebar.slider("Filter by Wickets", value=(wicket_by_player.min(), wicket_by_player.max()))
+
+wicket_by_player = wicket_by_player.sort_values(ascending=False)
+
+if wicket_value[0] == wicket_by_player.min() and wicket_value[1] == wicket_by_player.max():
+    filtered_wicket_by_player = wicket_by_player
+else:
+    wicket_by_player = wicket_by_player[wicket_by_player > wicket_value[0]]
+    wicket_by_player = wicket_by_player[wicket_by_player < wicket_value[1]]
+    filtered_wicket_by_player = wicket_by_player
+
+fig4 = px.bar(filtered_wicket_by_player, x = wicket_by_player.index, y = 'Wickets', color='Wickets', template='seaborn', color_continuous_scale='viridis')
+fig4.update_traces(width = 3)
+st.plotly_chart(fig4, use_container_width=True, height = 500)

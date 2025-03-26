fd03c344bbb039f1093df35437c08c80efb06da5
Saaeesh-Pednekar <macincredible369@gmail.com>
2025-03-26 17:43:54 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-import os 
 import warnings
 
 warnings.filterwarnings('ignore')
@@ -21,25 +20,15 @@ data_col = None
 # f1 = st.file_uploader(":file_folder: Upload a file", type=("csv", "txt", "xlsx", "xls"))
 st.sidebar.header("Filters")
 st.markdown('<style>[data-testid="stMarkdownContainer"]div.block-container{padding-top:0rem}</style>', unsafe_allow_html=True)
-data_col = st.sidebar.multiselect("Select a column for viewing datafield", [df_match_info_data.columns[x] for x in range(1, len(df_match_info_data.columns))])
+# data_col = st.sidebar.multiselect("Select a column for viewing datafield", [df_match_info_data.columns[x] for x in range(1, len(df_match_info_data.columns))])
 st.markdown('<style>[data-testid="stSidebar"]{background-color:#e6c4ff}</style>', unsafe_allow_html=True)
-st.markdown('<style>[data-testid="stSelectionbox"]{margin-bottom:2rem}</style>', unsafe_allow_html=True)
+# st.markdown('<style>[data-testid="stSelectionbox"]{margin-bottom:2rem}</style>', unsafe_allow_html=True)
 
 
-if data_col == []:
-    data_display = st.write(df_match_info_data[df_match_info_data.columns[1:]])
-else:
-    data_display = st.write(df_match_info_data[[x for x in data_col]])
-
-# team_options1 = df_match_info_data['team1'].unique()[0]
-# team_options2 = df_match_info_data['team1'].unique()[1]
-
-# team_options1 = [x for x in df_match_info_data['team1'].unique() if x !=team_options2]
-# team1 = st.sidebar.selectbox("Choose Team 1", team_options1)
-# st.markdown('<style>[data-testid="stWidgetLabel"]{padding-top:0rem}</style>', unsafe_allow_html=True)
-# st.sidebar.write("vs")
-# team_options2 = [x for x in df_match_info_data['team2'].unique() if x !=team_options1]
-# team2 = st.sidebar.selectbox("Choose Team 2", team_options2)
+# if data_col == []:
+#     data_display = st.write(df_match_info_data[df_match_info_data.columns[1:]])
+# else:
+#     data_display = st.write(df_match_info_data[[x for x in data_col]])
 
 
 col1, col2 = st.columns((2))
@@ -63,8 +52,6 @@ with col1:
     st.plotly_chart(fig1, use_container_width=True, height = 200)
 
 
-
-
 if team_name == []:
     toss_decision_wins = df_match_info_data.groupby(by=['toss_decision'])['winner'].count()
     toss_decision_wins = toss_decision_wins.rename_axis("Toss Decision")
@@ -77,7 +64,6 @@ else:
 
 with col2:
     st.subheader("Match Wins by Toss Decision")
-    # st.write(toss_decision_wins)
     fig2 = px.pie(toss_decision_wins, values=toss_decision_wins, names=toss_decision_wins.index, hole=0.65)
     fig2.update_traces(text = toss_decision_wins.index)
     st.plotly_chart(fig2, use_container_width=True, height = 200)
@@ -124,9 +110,6 @@ wicket_by_player = wicket_by_player.rename('Wickets')
 
 
 
-# st.write(wicket_by_player)
-
-
 wicket_value = st.sidebar.slider("Filter by Wickets", value=(wicket_by_player.min(), wicket_by_player.max()))
 
 wicket_by_player = wicket_by_player.sort_values(ascending=False)
@@ -138,7 +121,7 @@ else:
     wicket_by_player = wicket_by_player[wicket_by_player <= wicket_value[1]]
     filtered_wicket_by_player = wicket_by_player
 
-st.header("Most Wickets taken by Bowler")
+st.header("Most Wickets Taken by Bowler")
 fig4 = px.bar(filtered_wicket_by_player, x = wicket_by_player.index, y = 'Wickets', color='Wickets', template='seaborn')
 fig4.update_traces(width = 3)
 st.plotly_chart(fig4, use_container_width=True, height = 500)
@@ -150,7 +133,6 @@ else:
     matches_played_in_venue = filtered_venue_data.groupby(by=['venue'])['venue'].count()
 
 
-# st.write(matches_played_in_venue)
 
 matches_played_in_venue = matches_played_in_venue.rename_axis('Stadium')
 matches_played_in_venue = matches_played_in_venue.rename('Matches Played')
